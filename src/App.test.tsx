ea7212cc9c1b51f3e4f5c0e48043ed00fe87048f
Prashanth Balakrishnan <prashbala13@gmail.com>
2023-12-06 16:41:53 -0500

    added new tests to see if it fails

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,16 +1,13 @@
 // @vitest-environment jsdom
 
-import { render } from "../test/utilities";
+import { render, screen } from "../test/utilities";
 import App from "./App";
+
+// CHECKS FOR ACCESSIBILITY VIOLATIONS
+
 // import { axe, toHaveNoViolations } from "jest-axe";
 
 // expect.extend(toHaveNoViolations);
-
-describe("App", () => {
-  it("should render app", () => {
-    render(<App />);
-  });
-});
 
 // it("should have no accessibility violations", async () => {
 //   const { container } = render(<App />);
@@ -18,3 +15,15 @@ describe("App", () => {
 
 //   expect(results).toHaveNoViolations();
 // });
+
+describe("App", () => {
+  it("should render app", () => {
+    render(<App />);
+    screen.debug();
+  });
+  it("should have a title of TODO", () => {
+    render(<App />);
+    const title = screen.getByRole("heading", { name: /todso/i });
+    expect(title).toHaveTextContent(/todo/i);
+  });
+});
