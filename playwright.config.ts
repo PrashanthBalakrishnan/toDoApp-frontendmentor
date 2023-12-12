6fe9060360875717417f90398be0aa12f61e1b58
Prashanth Balakrishnan <prashbala13@gmail.com>
2023-12-12 18:39:28 -0500

    added e2e testing

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ import { defineConfig, devices } from "@playwright/test";
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: "./tests",
+  testDir: "./e2etests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
@@ -70,8 +70,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
+  //   command: "npm run start",
+  //   url: "http://127.0.0.1:3000",
   //   reuseExistingServer: !process.env.CI,
   // },
 });
