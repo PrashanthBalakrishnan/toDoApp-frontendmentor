84b66e43e1f535a4e701c5622dbd3873ec799078
Prashanth Balakrishnan <prashbala13@gmail.com>
2023-12-13 13:09:10 -0500

    added dnd featrure

--- src/components/todoFilter/TodoFilter.tsx
+++ src/components/todoFilter/TodoFilter.tsx
@@ -15,7 +15,6 @@ const TodoFilter: React.FC<TodoFilterProps> = ({ filter, setFilter }) => {
             value="all"
             type="radio"
             className="filter__radioBtn"
-            aria-label="show all filter"
             checked={filter === "all"}
             onChange={() => setFilter("all")}
           />
@@ -26,7 +25,6 @@ const TodoFilter: React.FC<TodoFilterProps> = ({ filter, setFilter }) => {
             name="filter"
             value="active"
             type="radio"
-            aria-label="Active To do filter"
             className="filter__radioBtn"
             checked={filter === "active"}
             onChange={() => setFilter("active")}
@@ -40,7 +38,6 @@ const TodoFilter: React.FC<TodoFilterProps> = ({ filter, setFilter }) => {
             name="filter"
             value="completed"
             type="radio"
-            aria-label="Complete To do filter"
             className="filter__radioBtn"
             data-testid="completed"
             checked={filter === "completed"}
