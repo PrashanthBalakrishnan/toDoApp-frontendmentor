57ceac8e8ff78fe94a1196e21cf89ddb288638e6
Prashanth Balakrishnan <prashbala13@gmail.com>
2023-12-26 15:12:51 -0500

    added playwright test

--- e2etests/basic.spec.tsx
+++ e2etests/basic.spec.tsx
@@ -1,4 +1,4 @@
-import { test } from "@playwright/test";
+import { test, expect } from "@playwright/test";
 
 test("Adding/editing/deleting a todo", async ({ page }) => {
   await page.goto("/");
@@ -12,7 +12,14 @@ test("Adding/editing/deleting a todo", async ({ page }) => {
   await page.getByTestId("delete-button").click();
 });
 
-test("Checking if filters work", async ({ page }) => {
+test("Filter Test", async ({ page }) => {
   await page.goto("/");
-  await page.pause();
+  await page.getByPlaceholder("Create a new task...").click();
+  await page.getByPlaceholder("Create a new task...").fill("go to gym");
+  await page.getByTestId("submit-button").click();
+  await page.getByLabel("Task", { exact: true }).check();
+  await page.getByLabel("filteractive").click();
+  expect(page.getByText("go to gym")).not.toBeVisible();
+  await page.getByLabel("filtercompleted").click();
+  await page.getByRole("button", { name: "clear completed" }).click();
 });
